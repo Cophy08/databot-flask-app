2795764567dcbc834117ff95432f2a76f5fef4c4
Kevin Kan <kan.kevin@gmail.com>
2015-09-10 01:11:43 -0400

    Remove trailing '/' from routing

--- databot/app/bye.py
+++ databot/app/bye.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 bye_page = Blueprint('blueprint_name_for_bye_page', __name__)
 
-@bye_page.route('/databot/bye/')
+@bye_page.route('/databot/bye')
 
 def bye_world():
 	return 'Bye World!'
